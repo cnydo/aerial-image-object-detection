fdc0803786e0f241ed05571efa8a24ec1b6ba1cd
An Thành Long <oraclevn8@gmail.com>
2023-12-05 01:51:57 +0700

    Add files via upload

--- rotate_wrong_oriented_img.py
+++ rotate_wrong_oriented_img.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 with open('list of wrong oriented image.txt', 'r') as f:
     image_paths = [line.strip() for line in f]
 
-for image_path_str in tqdm(image_paths):
+for image_path_str in (progress_bar:=tqdm(image_paths)):
+    progress_bar.set_description(f"Processing {image_path_str}")
     image_path = Path(image_path_str)
     if image_path.exists():
         img = cv2.imread(str(image_path))
