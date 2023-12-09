415e8a1a95c8212b22dad901f93fff95e6de7f1d
An Thành Long <oraclevn8@gmail.com>
2023-12-09 23:43:29 +0700

    Add path field

--- generate_yaml.py
+++ generate_yaml.py
@@ -3,19 +3,20 @@ import yaml
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--classes', type=str, required=True, help='Comma-separated list of class names')
+parser.add_argument('--classes', type=str, required=False, default="Zebra,Giraffe,Elephant" , help='Comma-separated list of class names')
 args = parser.parse_args()
 
 # Split the comma-separated list of class names
 class_names = args.classes.split(',')
 
 # Define your directories
-train_dir = '../train/images'
-val_dir = '../val/images'
-test_dir = '../test/images'
-
+train_dir = 'train/images'
+val_dir = 'val/images'
+test_dir = 'test/images'
+root_dir = '../data' # need to specify the path to data folder
 # Create a dictionary with your data
 data = {
+    'path': root_dir,
     'train': train_dir,
     'val': val_dir,
     'test': test_dir,
@@ -26,4 +27,4 @@ data = {
 # Write the dictionary to a YAML file
 with open('data.yaml', 'w') as outfile:
     yaml.dump(data, outfile, default_flow_style=False)
-    print("YAML file created successfully")
+    print("YAML file created successfully: data.yaml")
