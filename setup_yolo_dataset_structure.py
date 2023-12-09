151197956878f01384e83a490d752b727116492e
An Thành Long <oraclevn8@gmail.com>
2023-12-09 23:47:34 +0700

    Add files via upload

--- setup_yolo_dataset_structure.py
+++ setup_yolo_dataset_structure.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import shutil
 from pathlib import Path
+import comet_ml
 
 # Directories to process
 directories = ['train', 'val', 'test']
@@ -11,8 +12,10 @@ for directory in directories:
 
     # Get list of .jpg files in directory
     images = list(Path(directory).glob('*.jpg'))
-
+    if len(images) == 0:
+        print(f"No .jpg files found in {directory}")
+        continue
     # Move .jpg files to 'images' subdirectory with progress bar
     for image_path in (pbar:=tqdm(images)):
-        pbar.set_description(f"Moving {image_path.name} into {directory}/images")
+        pbar.set_description(f"Moving {image_path.name:30s} into {directory}/images")
         shutil.move(str(image_path), str(Path(directory, 'images', image_path.name)))
